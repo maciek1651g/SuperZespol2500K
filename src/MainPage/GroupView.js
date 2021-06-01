5d95c7882ef4eedc9c0241f58c458eb3c98cfa6e
Maciek <maciek1651g@gmail.com>
2021-06-01 22:16:10 +0200

    display groups, teams and courses

--- src/MainPage/GroupView.js
+++ src/MainPage/GroupView.js
@@ -13,127 +13,160 @@ import TabCard from "./TabCard";
 import $ from "./getElement";
 import CheckBox from "../LoginPage/CheckBox";
 
-export default class GroupView extends Component {
-  state = {
-    isPostsOpen: false,
-    isInfoOpen: false,
-    isChatOpen: false,
-    isUsersOpen: false,
-    coursesOrTeams: true,
-    selectedGroup: 0,
-    subpage: 0,
-    lesson: "",
-    groupNumber: 0,
-  };
-  openPosts(lesson, groupNumber) {
-    this.setState({
-      subpage: 1,
-      // isPostsOpen: !this.state.isPostsOpen,
-      lesson: lesson,
-      groupNumber: groupNumber,
-    });
-  }
-  changeSubpage(number) {
-    this.setState({
-      subpage: number,
-    });
-  }
-  changeGroup() {
-    this.setState({
-      selectedGroup: $("group").value,
-    });
-  }
-  changeCoursesOrGroup(){
-    this.setState({
-      coursesOrTeams: !this.state.coursesOrTeams,
-    });
-  }
-  render() {
-    let table = [];
-    if(this.props.gtable.length>0)
-    {
-      if(this.state.coursesOrTeams){
-        table = this.props.gtable[this.state.selectedGroup]["courses"];
-      } else {
-        table = this.props.gtable[this.state.selectedGroup]["teams"];
-      }
-    }
-    const groupsArray = this.props.gtable;
-    const setGroupsTable = this.props.setGTable;
-    let { isPostsOpen, lesson, groupNumber, subpage } = this.state;
-    return (
-      <div className={stylesMainPage.rightContent}>
-        {subpage === 1 ? (
-          <Posts
-            lesson={lesson}
-            groupNumber={groupNumber}
-            changeSubpage={this.changeSubpage.bind(this)}
-          />
-        ) : null}
-        {subpage === 2 ? (
-          <Chat
-            lesson={lesson}
-            groupNumber={groupNumber}
-            changeSubpage={this.changeSubpage.bind(this)}
-          />
-        ) : null}
-        {subpage === 3 ? (
-          <Users
-            lesson={lesson}
-            groupNumber={groupNumber}
-            changeSubpage={this.changeSubpage.bind(this)}
-          />
-        ) : null}
-        {subpage === 4 ? (
-          <Info
-            lesson={lesson}
-            groupNumber={groupNumber}
-            changeSubpage={this.changeSubpage.bind(this)}
-            mtable={table}
-          />
-        ) : null}
-        {subpage === 5 ? (
-          <GroupCalendar
-            lesson={lesson}
-            groupNumber={groupNumber}
-            changeSubpage={this.changeSubpage.bind(this)}
-          />
-        ) : null}
+const GroupView = (props) =>  {
+  const [isPostsOpen, setPostsOpen] = React.useState(false);
+  const [isInfoOpen, setInfoOpen] = React.useState(false);
+  const [isChatOpen, setChatOpen] = React.useState(false);
+  const [isUsersOpen, setUsersOpen] = React.useState(false);
+  const [coursesOrTeams, setCoursesOrTeams] = React.useState(true);
+  const [selectedGroup, setSelectedGroup] = React.useState(0);
+  const [subpage, setSubpage] = React.useState(0);
+  const [lesson, setLesson] = React.useState("");
+  const [groupNumber, setGroupNumber] = React.useState(0);
+  const [viewChoose, setViewChoose] = React.useState("0");
+  const [groupArrayState, setGroupArrayState] = React.useState(props.gtable);
+  const [table, setTable] = React.useState(props.gtable);
 
-        {subpage === 0 ? (
-          <>
-            <TopMainPage />
-            <GroupButton groupsArray={groupsArray} setGroupsArray={setGroupsTable}/>
-            <p
-              style={{
-                fontSize: "2em",
-                margin: "0",
-              }}
-            >
-              Twoje Grupy
-            </p>
-            <div className={stylesGroupView.topContainer}>
-              <div style={{display: "flex", flexDirection: "row", marginBottom: "20px",
-                width: "60%", justifyContent: "space-between"}}>
-                <div style={{marginRight: "20px"}}>
-                  <label htmlFor="group">Wybierz grupę: </label>
-                  <select id="group" onChange={this.changeGroup.bind(this)}>
-                    {
-                      groupsArray.map((value,key)=><option key={key+1} value={key}>{value.name}</option>)
-                    }
-                  </select>
-                </div>
-                <div>
-                  <CheckBox text="Przełącz widok" changeFunction={this.changeCoursesOrGroup.bind(this)}/>
-                </div>
-              </div>
-              <div className={stylesGroupView.groupContainer}>
-                <TabCard gtable={table} openPosts={this.openPosts.bind(this)}/>
-              </div>
-            </div>
-          </>
-        ) : null}
-      </div>
-    );
+
+  const openPosts = (lesson, groupNumber) => {
+    setSubpage(1);
+    setLesson(lesson);
+    setGroupNumber(groupNumber);
   }
+  const changeSubpage = (number) => {
+    setSubpage(number);
+  }
+  const changeGroup=() => {
+    setSelectedGroup($("group").value);
+  }
+  const changeViewChoose = () => {
+    setViewChoose($("view").value);
+  }
+  const changeCoursesOrGroup=()=>{
+    setCoursesOrTeams(!coursesOrTeams)
+  }
+
+  const changeView = () => {
+    switch (viewChoose)
+    {
+      case "0":
+        setTable(groupArrayState)
+        break;
+      case "1":
+        setTable(groupArrayState[selectedGroup]["teams"]);
+        break;
+      case "2":
+        setTable(groupArrayState[selectedGroup]["courses"]);
+        break;
+
+    }
+  }
+
+  React.useEffect(() => {
+    changeView();
+  }, [viewChoose, selectedGroup, groupArrayState]);
+
+  if(props.gtable!==groupArrayState)
+  {
+    setGroupArrayState(props.gtable);
+  }
+
+  const groupsArray = props.gtable;
+  const setGroupsTable = props.setGTable;
+
+  return (
+    <div className={stylesMainPage.rightContent}>
+      {subpage === 1 ? (
+        <Posts
+          lesson={lesson}
+          groupNumber={groupNumber}
+          changeSubpage={changeSubpage}
+        />
+      ) : null}
+      {subpage === 2 ? (
+        <Chat
+          lesson={lesson}
+          groupNumber={groupNumber}
+          changeSubpage={changeSubpage}
+        />
+      ) : null}
+      {subpage === 3 ? (
+        <Users
+          lesson={lesson}
+          groupNumber={groupNumber}
+          changeSubpage={changeSubpage}
+        />
+      ) : null}
+      {subpage === 4 ? (
+        <Info
+          lesson={lesson}
+          groupNumber={groupNumber}
+          changeSubpage={changeSubpage}
+          mtable={table}
+        />
+      ) : null}
+      {subpage === 5 ? (
+        <GroupCalendar
+          lesson={lesson}
+          groupNumber={groupNumber}
+          changeSubpage={changeSubpage}
+        />
+      ) : null}
+
+      {subpage === 0 ?
+        <>
+          <TopMainPage />
+          <GroupButton groupsArray={groupsArray} setGroupsArray={setGroupsTable}/>
+          <p
+            style={{
+              fontSize: "2em",
+              margin: "0",
+            }}
+          >
+            Twoje Grupy
+          </p>
+          <div className={stylesGroupView.topContainer}>
+
+
+
+
+            <div style={{display: "flex", flexDirection: "row", marginBottom: "20px",
+              width: "60%", justifyContent: "space-between"}}>
+
+              <div>
+                <label htmlFor="view">Wybierz widok: </label>
+                <select id="view" onChange={changeViewChoose}>
+                  <option value={0}>Grupy/Roczniki</option>
+                  <option value={1}>Zespoły</option>
+                  <option value={2}>Przedmioty</option>
+                </select>
+              </div>
+
+              <div style={{marginRight: "20px"}}>
+                <label htmlFor="group">Wybierz grupę: </label>
+                <select id="group" onChange={changeGroup} disabled={viewChoose==="0"}>
+                  {
+                    groupsArray.map((value,key)=><option key={key+1} value={key}>{value.name}</option>)
+                  }
+                </select>
+              </div>
+
+            </div>
+
+
+
+
+
+            <div className={stylesGroupView.groupContainer}>
+
+              <TabCard gtable={table} openPosts={openPosts}/>
+
+            </div>
+          </div>
+        </>
+       : null}
+    </div>
+  );
 }
+
+export default GroupView
